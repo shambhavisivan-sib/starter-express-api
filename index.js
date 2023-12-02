55da70d554d6d0f894549bcc6489e2b5c220c481
Shambhavi Sivan <shambhavisivan@Shambhavis-MacBook-Pro.local>
2023-12-02 19:24:09 +0530

    send request object

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 app.all("/", (req, res) => {
   console.log("Just got a request!", req);
-  res.send("Yo its workingggg !");
+  res.send(`Yo its workingggg !: ${req}`);
 });
 app.listen(process.env.PORT || 3000);
